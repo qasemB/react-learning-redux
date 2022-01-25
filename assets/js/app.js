98340689826c40ddd8e3c60c150a8667d94366ab
qasemB <mahdicmptr@gmail.com>
2022-01-25 16:27:06 +0330

    part #56

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,60 @@
-const states = {
-    apple: 5
+const fruitStates = {
+    apple: 5,
+    orange: 10
+}
+const foodStates = {
+    sandwich: 15
 }
 
-const reducer = (state = states , action )=>{
+const fruitReducer = (state = fruitStates , action )=>{
     switch (action.type) {
         case "BUY_APPLE":
-            return {apple: state.apple -1}
+            return {...state , apple: state.apple -1}
+        case "BUY_ORANGE":
+            return {...state , orange: state.orange -1}
         default:
             return state
     }
 }
 
-const store = Redux.createStore(reducer);
+const foodReducer = (state = foodStates , action )=>{
+    switch (action.type) {
+        case "BUY_SANDWICH":
+            return {...state , sandwich: state.sandwich -1}
+        default:
+            return state
+    }
+}
+
+const rootReducer = Redux.combineReducers({
+    fruit: fruitReducer,
+    food: foodReducer
+})
+
+const store = Redux.createStore(rootReducer);
+
+const render = ()=>{
+    console.log(store.getState());
+    document.getElementById('apple_count').innerHTML = "تعداد سیب ها :" + store.getState().fruit.apple
+    document.getElementById('orange_count').innerHTML = "تعداد پرتقال ها :" + store.getState().fruit.orange
+    document.getElementById('sandwich_count').innerHTML = "تعداد ساندوسچ ها :" + store.getState().food.sandwich
+}
 
 store.subscribe(()=>{
-    document.getElementById('apple_count').innerHTML = "تعداد سیب ها :" + store.getState().apple
+    render()
 })
 
 document.getElementById('buy_apple').addEventListener('click' , ()=>{
     store.dispatch({type:"BUY_APPLE"})
 })
+document.getElementById('buy_orange').addEventListener('click' , ()=>{
+    store.dispatch({type:"BUY_ORANGE"})
+})
+document.getElementById('buy_sandwich').addEventListener('click' , ()=>{
+    store.dispatch({type:"BUY_SANDWICH"})
+})
 
-document.getElementById('apple_count').innerHTML = "تعداد سیب ها :" + store.getState().apple
+render()
 
 
 
